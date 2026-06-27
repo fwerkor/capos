1872b36b7d33625f75f4455c0ea50aea9138469c
Castronaut <3116935098@qq.com>
2026-06-27 09:28:05 +0800

    armsr: build arm64 EFI zboot kernels

--- target/linux/armsr/armv8/target.mk
+++ target/linux/armsr/armv8/target.mk
@@ -1,7 +1,7 @@
 ARCH:=aarch64
 SUBTARGET:=armv8
 BOARDNAME:=64-bit (armv8) machines
-KERNELNAME:=Image.gz
+KERNELNAME:=Image.gz vmlinuz.efi
 
 define Target/Description
   Build multi-platform images for the ARMv8 instruction set architecture
